7a538ae21df71e8f47d9b03ac8a1a72653c0d9d2
Giri Veeramani <giriveeramani2006@gmail.com>
2026-05-17 19:25:23 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://codesherlock-x-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -20,6 +21,7 @@ app.include_router(chat.router)
 app.include_router(pr.router)
 app.include_router(docs.router)
 app.include_router(tests.router)
+
 
 @app.get("/")
 def root():
